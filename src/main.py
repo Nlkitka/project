43919de8a28dd1ef0bfb3fa04cbcad66609ab8b0
nikita <mr.boss_1980@mail.ru>
2022-06-22 22:37:24 +0300

    code + dvc

--- src/main.py
+++ src/main.py
@@ -2,15 +2,10 @@ from src import parser
 from src import preprocessing
 from src import receive
 
-
-def print_hi(name):
+if __name__ == '__main__':
     pars_info = parser.ParsAllId()
     pars_info.pars_id_posts()
     pars_info.get_posts_text_and_id()
     text_check_add = preprocessing.ClearText(pars_info.all_texts_and_id_post)
     print(text_check_add.get_result())
     receive.send_mes(text_check_add.get_result())
-
-
-if __name__ == '__main__':
-    print_hi('PyCharm')
